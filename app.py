ebe3147fca569c1bb045f70cc8de7d82d5bff857
kasul <sumitpujari199723@gmail.com>
2022-03-27 17:50:08 +0530

    database display feature api created

--- app.py
+++ app.py
@@ -60,3 +60,16 @@ def url_redirect(id):
         flash("Invalid URL")
         return redirect(url_for("index"))
 
+@app.route('/display')
+def display():
+    conn = get_db_connection()
+    db_urls = conn.execute("SELECT id, created, original_url, clicks FROM urls").fetchall()
+    conn.close()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url["short_url"] = request.host_url + hashids.encode(url["id"])
+        urls.append(url)
+
+    return render_template("display.html", urls = urls)
